4d5f3d979f9748c5f355f8c42ab6030e3493c8d7
JeanPaiva42 <jean.paiva42@gmail.com>
2017-10-02 11:29:57 -0300

    Add files via upload

--- csma-cd.py
+++ csma-cd.py
@@ -1,5 +1,5 @@
 import random as rand
-
+import matplotlib.pyplot as plt
 
 class Falantes:
     name = None
@@ -8,8 +8,8 @@ class Falantes:
 
     def setName(self, n):
         self.name = n
-    def setRandInt(self):
-        self.randInt = rand.randint(0,100)
+    def setRandInt(self, r):
+        self.randInt = rand.randint(0,r)
     def setFala(self, f):
         self.fala = f
     def getName(self):
@@ -20,66 +20,84 @@ class Falantes:
         return self.fala
 
 
-def auxCsmaCd(vetorzin):
-    a=None
+def auxCsmaCd(vetorzin, vaiFalar):
+    a = None
+    add=0
     proxVets = vetorzin
+    queroFalar = vaiFalar
     if proxVets[0]:
         if(len(proxVets[0])>0):
             a = rand.randint(0, len(proxVets[0])-1)
         else:
             a = rand.randint(0,0)
-            print proxVets
-        print str(a) + ' '+ str(len(proxVets[0]))
-        print proxVets
-        print 'Falador diz.... iiadia ' + proxVets[0][a].getName()
+        print 'Falador diz.... ' + proxVets[0][a].getName()
+        if proxVets[0][a] in queroFalar:
+            print proxVets[0][a].getName() + ' finalmente falou!!'
+            add+=1
         proxVets[0].pop(a)
     for i in range(0, len(proxVets)):
     #add vetor de vetor e etc
         c = []
         proxVets.append(c)
-    print 'vetor acrescido '
-    print proxVets
     for j in proxVets[0]:
         randVec = rand.randint(1, len(proxVets)-1)
         proxVets[randVec].append(j)
-    print 'antes '
-    print proxVets
-    proxVets.pop(0)
-    print 'dps '
-    print proxVets
-    return proxVets
+    return [proxVets, queroFalar, add]
 
 
 
 
 
-def csmaCd():
+def csmaCd(canal, chances):
+    #A simulacao poderia ser um pouco mais leve, mas estou tentando fazer com que as coisas se visualizem melhor
     colisoes = [[]]
-    nomes = ['romario','ronaldin','pele','cloud']
+    vaiFalar = []
+    numFalante=0
+    numTentouFalar=0
+    nomes = ['mario','Tifa','MegaMan','Cloud', 'Lightning', 'Squall', 'Zidane', 'Vivi', 'Barret', 'Terra']
+    for i in range(0, chances):
+        a = rand.randint(0, 9)
+        nomes.append(nomes[a]+str(i))
     falador = []
-    for i in range(0,3):
+    for i in range(0, len(nomes)):
         o = Falantes()
         o.setName(nomes[i])
         falador.append(o)
 
-    for p in range(0, 100):
-        for f in falador:
-            f.setRandInt()
-        for f in falador:   
-            if(f.getRandInt() <= 30):
-                colisoes[0].append(f)
-        #if(len(colisoes[0]) == 1):
-            #print 'Falador diz.... ' + colisoes[0][0].getName()
-            #sinal = True
-        colisoes = auxCsmaCd(colisoes)     
+    for p in range(0, chances):
+        
             
+        for f in falador:
+            f.setRandInt(chances)
+            if(f.getRandInt() <= canal):
+                colisoes[0].append(f)
+                vaiFalar.append(f)
+            
+                print f.getName()+ " quer falar..."
+                numTentouFalar+=1
+        vets = auxCsmaCd(colisoes, vaiFalar)
+        colisoes = vets[0]
+        vaiFalar = vets[1]
+        numFalante+= vets[2]
        
        
-    print colisoes
     print 'Finalizando protocolo...'
-
+    print str(numTentouFalar)+" "+ str(numFalante)
+    return [numTentouFalar, numFalante]
 
 print 'Iniciando protocolo...'
-csmaCd()
-    
+pl = csmaCd(1, 100)
+labels = ['Tentaram Falar', 'Conseguiram Falar']
+sizes = [pl[0],pl[1]]
+colors = ['gold', 'yellowgreen']
+explode = (0.1, 0)  
+ 
+# Plot
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=140)
+ 
+plt.axis('equal')
+plt.show()
+plt.ylabel('colisoes atraves do tempo')
+
     
